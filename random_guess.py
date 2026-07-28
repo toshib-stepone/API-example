3fa4678bd220ae954e5fb01bd67d428b207cdad6
toshibalone512 <143947745+toshibalone512@users.noreply.github.com>
2026-07-28 13:01:53 +0530

    random guess game

---
+++ random_guess.py
@@ -0,0 +1,21 @@
+import random
+
+def guessing_game():
+    number = random.randint(1, 100)
+    attempts = 0
+
+    print("I'm thinking of a number between 1 and 100.")
+
+    while True:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+        else:
+            print(f"Correct! You guessed it in {attempts} attempts.")
+            break
+
+guessing_game()
